589091084a06356af3e4a6cbe3bb83810b9293c7
kallesedergren <kalle.sedergren@gmail.com>
2016-02-10 10:59:16 +0200

    Demo0512

---
+++ demo05_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo05_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cd-luokka
+            //-artisti
+            //-hinta
+            //-List<Biisi> biisit, biisit-tietorakenne
+            //-AddBiisi
+            //-PrintData
+
+            //biisi-luokka
+            //-nimi
+            //-pituus
+
+            //PO
+            //luo CD-olio
+            //luo parit biisit
+            //Lisää CD:hen
+            //Tulosta CD:n sisältö
+        }
+    }
+}
