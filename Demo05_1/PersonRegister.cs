c79b53376d3d1a95e2d53d8e5a72e46932b19b60
kallesedergren <kalle.sedergren@gmail.com>
2016-02-10 10:50:19 +0200

    Demo05.

---
+++ Demo05_1/PersonRegister.cs
@@ -0,0 +1,89 @@
+﻿/* 
+ * Copyright (C) KMS
+ *
+ * Part of Person Application project.
+ *
+ * Creation: 10.2.2016
+ * Author: Kalle Sedergren
+ *
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05_1
+{
+    /// <summary>
+    /// This class holds peson objects in a collection.
+    /// </summary>
+    class PersonRegister
+    {
+        /// <summary>
+        /// Person List stores person objects.
+        /// </summary>
+        private List<Person> persons; 
+
+        /// <summary>
+        /// Default constructor initializes a collection.
+        /// </summary>
+        public PersonRegister()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Adds a new person to collection. 
+        /// </summary>
+        /// <param name="person">person to added</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        /// <summary>
+        /// Returns a person to caller application. 
+        /// </summary>
+        /// <param name="index">person index in collection</param>
+        /// <returns>person object</returns>
+
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+            {
+                return persons.ElementAt(index);
+            } else
+            {
+                return null;
+            }
+        }
+
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            //for(int i=0;i<persons.Count;i++)
+
+            foreach(Person person in persons)
+            {
+                //is sotus same
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person; //Person found with that sotu.
+                }
+            }
+            return null; //Person not found.
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("******************************* \n");
+            Console.WriteLine("* My awesome collection * \n");
+            Console.WriteLine("******************************* \n");
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Demo05_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a register
+            PersonRegister myFriends = new PersonRegister();
+
+            //Create a few persons
+            Person person1 = new Person();
+            person1.Firstname = "Matti";
+            person1.Lastname = "Mainio";
+            person1.SocialSecurityNumber = "091209-235G";
+
+
+            Person person2 = new Person { Firstname = "Kirsi", Lastname = "Kernel", SocialSecurityNumber = "893423-855O" };
+
+
+            //Add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+
+
+            //Get one person from register
+            Person person3 = myFriends.GetPerson(1); // index - 0
+            if (person3 != null)
+            {
+                Console.WriteLine(person3.ToString());
+            } else
+            {
+                Console.WriteLine("Can not find person in that index.");
+            }
+
+
+            string sotu = "091209-235G4325";
+            Console.WriteLine("Finding person with sotu: " + sotu);
+            Person person4 = myFriends.FindPerson(sotu);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Person can't be found with that sotu. \n");
+            }
+
+            myFriends.PrintData();
+        }
+    }
+}
